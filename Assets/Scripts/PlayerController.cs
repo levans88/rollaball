89d9bc808dd8a0d2fb82ead9b9ba8851792f3463
Lenny Evans <lenny.evans3@gmail.com>
2016-07-24 19:33:10 -0500

    Complete tutorial 7 of 8, UI text.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     // Public variables will show up in the inspector
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     // Called on the first frame in which the script is active
     void Start() {
         // Retrieve the Rigidbody for phsics if it exists
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
 	// Update has an irregular timeline (time to process frames can vary)
@@ -31,6 +38,15 @@ public class PlayerController : MonoBehaviour {
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Pick Up")) {
             other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12) {
+            winText.text = "You Win!";
         }
     }
 }
