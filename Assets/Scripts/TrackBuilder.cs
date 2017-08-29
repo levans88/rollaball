d798328fbbdf87d9bb7f60261feca0974a4320c8
Lenny <lenny.evans3@gmail.com>
2017-08-28 23:41:43 -0500

    Track creation script in progress.

---
+++ Assets/Scripts/TrackBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackBuilder : MonoBehaviour {
+	[HideInInspector]
+	public Transform trackSegment;
+
+	public Texture2D sourceImage;
+	public int widthInTiles;
+	
+
+
+	// Use this for initialization
+	void Start () {
+		// Texture coordinates start at lower left corner
+		Color32[] pixelsArray = sourceImage.GetPixels32();
+
+		Debug.Log(pixelsArray);
+
+		//foreach (var thing in pixelsArray) {
+			//print("[" + thing.r + "]" + "[" + thing.g + "]" + "[" + thing.b + "]" );	
+			//print(thing);
+		//}
+
+		//Instantiate(, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+}
